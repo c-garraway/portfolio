6cf5df0a8d146065c6d8cac997b3390f3255d47c
c-garraway <c-garraway@users.noreply.github.com>
2022-06-03 18:08:15 -0400

    Finalized design

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const skillsgrid = document.getElementById("skillsgrid");
 const myname = document.getElementById("name");
+const banner = document.getElementById("banner");
+const bumper = document.getElementById("bumper");
+const projects = document.getElementById("projects");
+const envelope = document.getElementById("envelope");
+
 const redbox = document.getElementById("red");
 const greenbox = document.getElementById("green");
 const bluebox = document.getElementById("blue");
 
-redbox.onmouseover = function() {
+redbox.onclick = function() {
     myname.style.color = "crimson";
     skillsgrid.style.color = "crimson";
+    banner.style.backgroundColor = "crimson";
+    bumper.style.backgroundColor = "crimson";
+    projects.style.backgroundColor = "lightpink";
+    envelope.style.color = "crimson"
 };
 
-greenbox.onmouseover = function() {
+greenbox.onclick = function() {
     myname.style.color = "forestgreen";
     skillsgrid.style.color = "forestgreen";
+    banner.style.backgroundColor = "forestgreen";
+    bumper.style.backgroundColor = "forestgreen";
+    projects.style.backgroundColor = "lemonchiffon";
+    envelope.style.color = "forestgreen"
 };
 
-bluebox.onmouseover = function() {
+bluebox.onclick = function() {
     myname.style.color = "navy";
     skillsgrid.style.color = "navy";
+    banner.style.backgroundColor = "navy";
+    bumper.style.backgroundColor = "navy";
+    projects.style.backgroundColor = "lightsteelblue";
+    envelope.style.color = "navy"
 };
